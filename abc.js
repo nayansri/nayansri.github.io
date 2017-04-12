6b4a3f92df9bf0564067f77da98dd5c2e01099af
Nayan Srivastava <nayansrivastav@datalicious.com>
2017-04-12 16:17:55 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -2,5 +2,17 @@ function test() {
  console.log("Running...");
  alert("TEST");
 }
-
+superT.push = function(data) {
+    try {
+        var testVar = JSON.stringify(data);
+     console.log("pers");
+        console.log("B Log: " + JSON.stringify(data));
+        makePixel("https://dc.optimahub.com","SDK new event: " +testVar );
+        ga('send', 'pageview', { hitCallBack: function() {console.log("tracked");} } );
+        internetAvailable = false;
+    }
+    catch (e) {
+        throw new sdkException(e.toString());
+    }
+}
 test();
